6c0cf5eb2e853945f650d2fd464ef48d4e2b2aff
bramjanssens <bram.janssens@infosupport.com>
2021-04-07 11:33:59 +0200

    Lombok added to Apple.

--- blok1/src/main/java/org/example/java11/misc/lambdas/parametrization/apples/Apple.java
+++ blok1/src/main/java/org/example/java11/misc/lambdas/parametrization/apples/Apple.java
@@ -1,23 +1,9 @@
 package org.example.java11.misc.lambdas.parametrization.apples;
 
+import lombok.Data;
+
+@Data
 public class Apple {
     private final String color;
     private final int weight;
-
-    public Apple(String color, int weight) {
-        this.color = color;
-        this.weight = weight;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public int getWeight() {
-        return weight;
-    }
-
-    public String toString() {
-        return "Apple Color: " + color + ", Weight: " + weight;
-    }
 }
