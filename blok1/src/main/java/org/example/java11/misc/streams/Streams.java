e50812e8ba439030db1046b2a7f75971545b2310
bramjanssens <bram.janssens@infosupport.com>
2021-04-07 15:46:28 +0200

    Streams example extended again.

--- blok1/src/main/java/org/example/java11/misc/streams/Streams.java
+++ blok1/src/main/java/org/example/java11/misc/streams/Streams.java
@@ -2,8 +2,11 @@ package org.example.java11.misc.streams;
 
 import org.example.java11.slides.h3.Person;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
 
 public class Streams {
     public static void main(String[] args) {
@@ -36,11 +39,36 @@ public class Streams {
                 .forEach(System.out::println) // method reference
         ;
 
+        // van leeftijden naar personen naar één eindpersoon:
         Person reduced = Stream.of(10, 20, 30, 40)
                 .map(Person::new)
                 .reduce(new Person(), (totalPerson, p) -> totalPerson.plus(p.getName(), p.getAge()));
 
         System.out.println(reduced);
+
+        // van leeftijden naar personen naar een lijst met "zelf" collecten:
+        List<Person> collect = Stream.of(10, 20, 30, 40)
+                .map(Person::new)
+                .collect(
+                        () -> new ArrayList<>(), // generator voor nieuwe lege lijst van persons
+                        (people, p) -> people.add(p),  // wat hij elk stapje moet doen met de lijst en een person
+                        (chunk1, chunk2) -> chunk1.addAll(chunk2) // wat hij moet doen om twee lijsten te combineren bij parallel processing
+                );
+
+        // idem, refactored
+        List<Person> collectRefactored = Stream.of(10, 20, 30, 40)
+                .map(Person::new)
+                .collect(
+                        ArrayList::new,     // generator voor initiële lege lijst van persons
+                        ArrayList::add,     // wat hij elk stapje moet doen met de lijst en een person
+                        ArrayList::addAll   // wat hij moet doen om twee lijsten te combineren bij parallel processing
+                );
+
+        // idem met collect(toList()):
+        List<Person> collect2 =
+                Stream.of(10, 20, 30, 40)
+                        .map(Person::new)
+                        .collect(toList());
     }
 
 }
