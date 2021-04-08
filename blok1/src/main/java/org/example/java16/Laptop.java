59aecb0addb140f927b2e84f85a580bb47f8ec86
bramjanssens <bram.janssens@infosupport.com>
2021-04-08 09:43:19 +0200

    toolchains_example.xml and instructions added.

--- blok1/src/main/java/org/example/java16/Laptop.java
+++ blok1/src/main/java/org/example/java16/Laptop.java
@@ -1,6 +1,11 @@
 package org.example.java16;
 
-// public record Laptop(String brand, BigDecimal price) {
+// Java >= 15
 public class Laptop {
+// public record Laptop(String brand, BigDecimal price) {
+
+    // public Laptop(String brand, double price) {
+    //     this(brand, BigDecimal.valueOf(price));
+    // }
 
 }
--- blok1/src/main/java/org/example/java16/LaptopTester.java
+++ blok1/src/main/java/org/example/java16/LaptopTester.java
@@ -3,8 +3,9 @@ package org.example.java16;
 public class LaptopTester {
 
     public static void main(String[] args) {
-        // Laptop laptop = new Laptop("Dell", BigDecimal.valueOf(899.99));
+        // Laptop laptop = new Laptop("Dell", 899.99);
         // String brand = laptop.brand();
         // System.out.println(laptop);
+        // System.out.println(brand);
     }
 }
