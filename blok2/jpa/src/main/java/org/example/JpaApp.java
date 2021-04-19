35598a49789a57b62e810a9d47fda2b760678507
bramjanssens <bram.janssens@infosupport.com>
2021-04-19 11:49:22 +0200

    JPA added.

---
+++ blok2/jpa/src/main/java/org/example/JpaApp.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class JpaApp {
+
+    public static void main(String[] args) {
+        new JpaApp().start();
+    }
+
+    private void start() {
+        PersonDao personDao = new PersonDao();
+
+        Person person = personDao.find(1);
+        System.out.println(person);
+
+        personDao.save(new Person(2, "Piet", 42));
+
+    }
+}
