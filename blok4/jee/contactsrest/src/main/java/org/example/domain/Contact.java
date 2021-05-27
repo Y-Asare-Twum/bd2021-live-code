8c0b31c687e8273b61f74efd103a5ec93e54cfdd
bramjanssens <bram.janssens@infosupport.com>
2021-05-27 10:31:22 +0200

    App contactsrest added.

---
+++ blok4/jee/contactsrest/src/main/java/org/example/domain/Contact.java
@@ -0,0 +1,16 @@
+package org.example.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @Builder @AllArgsConstructor @NoArgsConstructor
+public class Contact {
+    private Long id;
+
+    private String name;
+
+    private int age;
+
+}
---
+++ blok4/jee/contactsrest/src/main/java/org/example/resources/ContactsResource.java
@@ -0,0 +1,55 @@
+package org.example.resources;
+
+import org.example.domain.Contact;
+
+import javax.ws.rs.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+
+@Path("/contacts")
+public class ContactsResource {
+
+    Contact.ContactBuilder cb = Contact.builder();
+
+    private final List<Contact> contacts = new ArrayList<>(List.of(
+            cb.name("Bram").age(41).id(1L).build(),
+            cb.name("Joop").age(23).id(2L).build(),
+            cb.name("Mieke").age(45).id(3L).build()
+    ));
+
+    // uri: .../contacts
+    //      .../contacts?name=Bram
+
+    @GET
+    @Produces(APPLICATION_JSON)
+    public List<Contact> get(@QueryParam("name") String n) {
+        return n == null ?
+                this.contacts :
+                contacts.stream()
+                        .filter(c -> c.getName().contains(n))
+                        .collect(toList());
+    }
+
+    // ...../contacts/1
+
+    @GET @Path("{id}")
+    @Produces(APPLICATION_JSON)
+    public Contact getById(@PathParam("id") Long id) {
+        return this.contacts.stream()
+                .filter(c -> c.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new BadRequestException(String.format("Contact with id %s not found.", id)));
+    }
+
+    @POST
+    @Produces(APPLICATION_JSON)
+    @Consumes(APPLICATION_JSON)
+    public Contact post(Contact c) {
+        this.contacts.add(c);
+        return c;
+    }
+
+}
